830d66b91c233712672b3c8e8170ac432447b098
anf17 <andriifedkin22@gmail.com>
2023-01-05 21:03:35 +0200

    node-typescript-lesson_11 - clusters

--- server.ts
+++ server.ts
@@ -1,19 +1,53 @@
-import * as util from 'util';
-import * as fs from 'fs';
-import * as https from 'https';
+import cluster from 'node:cluster';
+import * as http from 'http';
+import * as os from 'os';
 
-const readFile = util.promisify(fs.readFile);
-
-async function startServer() {
-  const [key, cert] = await Promise.all([readFile('key.pem'), readFile('certificate.pem')]);
-  https
-    .createServer({ key, cert }, (req, res) => {
-      res.statusCode = 200;
-      res.end('hello world');
+const numberOfCores = os.cpus().length;
+/*
+if (cluster.isPrimary) {
+  console.log(`Master ${process.pid} started`);
+  for (let i = 0; i < numberOfCores; i++) {
+    cluster.fork();
+  }
+} else {
+  http
+    .createServer((req, res) => {
+      res.writeHead(200);
+      res.end(`Process ${process.pid} says hello!`);
     })
-    .listen(8000, () => {
-      console.log('Server started');
-    });
+    .listen(8000);
+  console.log(`Worker ${process.pid} started`);
+}
+*/
+
+/*
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} started`);
+  for (let i = 0; i < numberOfCores; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', (worker) => {
+    console.log(`worker ${worker.process.pid} stopped working`);
+    cluster.fork();
+  });
+  cluster.on('fork', (worker) => {
+    console.log(`Worker ${worker.process.pid} started`);
+  });
+} else {
+  http
+    .createServer((req, res) => {
+      res.writeHead(200);
+      res.end(`Process ${process.pid} says hello!`);
+    })
+    .listen(8000);
 }
 
-startServer();
+*/
+if (cluster.isMaster) {
+  const worker = cluster.fork();
+  worker.on('message', (text) => {
+    console.log(text);
+  });
+} else {
+  process.send('Hello!');
+}
